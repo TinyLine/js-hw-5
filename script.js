a4ce99a271087406d163410edc51b85814dd3c9b
Діма Якименко <akimenkodima154@gmail.com>
2023-03-08 11:24:43 +0000

    master

---
+++ script.js
@@ -0,0 +1,108 @@
+
+// Задача №1
+
+// if else,
+
+// Написати програму де ми питаємо у користувача
+// номер місяця і показуємо до якого часу року він належить
+// Якщо користувач ввів 2 - показуєм слово Зима
+// Якщо 3 -Весна і т.д.
+
+//const maus = Number(promt('Яка пора року'))
+
+//if (maus === 12 || gnge.aer.erb;maus === 1 || maus === 2) {
+//    console.log('Зима')
+//} else if (maus >= 3 && maus <= 5) {
+//    console.log('Весна')
+//} else if (maus >= 6 && maus <= 8) {
+//    console.log('Літо')
+//} else if (maus >= 7 && maus <= 9) {
+//    console.log('Осінь')
+//} else{
+//    console.log('Error')
+//}
+
+// ............
+
+// Задача №2
+
+// Написати програму яка перевіряє логін і пароль користувача
+//  і виводить привітання - якщо все добре
+//  і якщо ні помилку - невінний логін
+
+//const login = 'user@gmail.com';
+//const password = '12345'
+//const userlogin = prompt('Введіть логін')
+//const userPassworld = prompt('Введіть пароль')
+
+//if (login === userLogin && password === userPassworld){
+//    console.log('Доступ відкрито')
+//} else {
+//    console.log('Відхилити')
+//}
+    
+
+// ............
+
+// Задача №3
+
+/* на for
+ * Напиши скрипт який підрахує суму всіх парних чисел,
+ * які входят в діапазон чисел в змінних від min до max.
+ * Наприклад, якщо min=0 і max=5, то діапазон 0-5,
+ * зроби перевірку
+ * виведи парні числа
+ * і порахуй їх суму, змінна total
+ * виведи консоль
+ */
+ //const min = 0;
+ //const max = 5;
+//let total = 0;
+//for (let i = min; i <= max; += 1) {
+   // if (i % 2 === 0)
+   // {
+   //     console.log("total",);
+   //     total+= i;
+   // }
+//}
+//console.log("Сумма")
+//..............................
+
+// Задача №4
+
+// Порахувати загальну суму в корзині
+//
+//const cart = [54, 28, 105, 70, 92, 17, 120];
+//let total = 0
+
+//for (let i = 0; i <cart.length; i += 1) {
+    //console.log(cart[i])
+    //total += cart[i]
+
+    //console.log(total)
+//}
+// ...........................
+
+// Задача №5
+
+// Напиши скрипт який підраховує суму усіх парних чисел в масиві
+// const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 14];
+//let total = 0
+
+//for (let i = 0; i < number.length; i += 1){
+//   console.log(number[i])
+//   if (numbers[i] % 2 === 0) {
+//        console.log('Парні числа')
+//       total += numbers[i]
+//   }
+//}
+//console.log(total)
+
+//for (const numbers of numbers) {
+//    if (number % 2 === 0) {
+//        console.log('Парні числа');
+//        total += number;
+//    }
+        
+//}
+//console.log(total);
